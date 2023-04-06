e948ba5797b57288b8b14d5b1e3dbee430134ab8
tsunchun <cunjun.yu@gmail.com>
2023-04-06 21:12:54 +0800

    add progress bar
    Signed-off-by: tsunchun <cunjun.yu@gmail.com>

--- static/js/index.js
+++ static/js/index.js
@@ -76,3 +76,27 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+const progress = document.getElementById("progress");
+const timer = document.getElementById("timer");
+button = document.getElementById("play");
+
+function progressLoop() {
+  setInterval(function () {
+    progress.value = Math.round((video.currentTime / video.duration) * 100);
+    timer.innerHTML = Math.round(video.currentTime) + " Minutes";
+  });
+}
+
+function playPause() {
+  if (video.paused) {
+    video.play();
+    button.innerHTML = "&#10073;&#10073;";
+  } else {
+    video.pause();
+    button.innerHTML = "►";
+  }
+}
+
+button.addEventListener("click", playPause);
+video.addEventListener("play", progressLoop);
